11eb5658ce2a2c14dd3c02a3071dbef8911eec2c
sunxp <xpsun@qq.com>
2023-09-20 16:19:16 +0800

    feat: 实现了对任务列表的增删改

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <script setup lang="ts">
+import {reactive, ref} from 'vue'
 
+interface TaskItem {
+    content: string;
+    isDone: boolean;
+}
+
+const list: TaskItem[] = reactive([
+    {content: "test1", isDone: false},
+    {content: "test2", isDone: false}
+]);
+
+let taskContent = ref("");
+
+function addNewTask(){
+    const newTask = {content: taskContent.value, isDone: false};
+    list.push(newTask);
+    taskContent.value = "";
+}
+function deleteTask(index: number){
+    list.splice(index,1);
+}
 </script>
 
 <template>
+    <h1>Todo List</h1>
+    <div>
+        <input type="text" v-model="taskContent">
+        <button @click="addNewTask()">添加</button>
+    </div>
+    <div>
+        <div v-for="(item, index) in list" class="taskItem">
+            <input type="checkbox" v-model="item.isDone">
+            <div :class="{isTaskDone: item.isDone}">{{item.content}}</div>
+            <button @click="deleteTask(index)">删除</button>
+        </div>
+    </div>
 
 </template>
 
 <style scoped>
+.taskItem {
+    display: flex
+}
 
+.isTaskDone {
+    text-decoration: line-through;
+}
 </style>
