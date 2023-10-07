926dfa9caab4b52fdcbf3106e1914912c4bd5bee
sunxp <xpsun@qq.com>
2023-10-07 17:43:19 +0800

    refactor: 重构1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import {reactive, ref, watch} from 'vue'
+import TaskInput from '@/TaskInput.vue'
+import TaskList from '@/TaskList.vue'
 
 interface TaskItem {
     content: string;
@@ -8,7 +10,7 @@ interface TaskItem {
 
 const list: TaskItem[] = reactive([]);
 
-let taskContent = ref("");
+
 
 let strInStorage = localStorage.getItem('list')??'[]'
 list.push(...JSON.parse(strInStorage));
@@ -18,41 +20,25 @@ watch(list, ()=>{
     console.log("list 被修改");
 })
 
-function addNewTask(){
-    const newTask = {content: taskContent.value, isDone: false};
+function addNewTask(taskContent: string){
+    const newTask = {content: taskContent, isDone: false};
     list.push(newTask);
-    taskContent.value = "";
+
 }
 
-function deleteTask(index: number){
-    list.splice(index,1);
-}
+
 
 
 </script>
 
 <template>
     <h1 class="text-red text-2xl">Todo List</h1>
-    <div class="flex w-20em gap-x-2">
-        <el-input type="text" v-model="taskContent" />
-        <el-button type="primary" @click="addNewTask()"><i class="i-ant-design-plus-circle-outlined"></i> 添加</el-button>
-    </div>
-    <div v-auto-animate>
-        <div v-for="(item, index) in list" class="taskItem">
-            <el-checkbox type="checkbox" v-model="item.isDone" />
-            <div :class="{isTaskDone: item.isDone}" class="grow">{{item.content}}</div>
-            <el-button type="danger" size="small" @click="deleteTask(index)"><i class="i-ant-design-delete-outlined"></i> 删除</el-button>
-        </div>
-    </div>
+
+    <task-input @newTask="addNewTask"></task-input>
+
+    <task-list :tasklist="list"></task-list>
 
 </template>
 
 <style scoped>
-.taskItem {
-    --at-apply: flex p-2 gap-x-2 items-center w-20em
-}
-
-.isTaskDone {
-    --at-apply: line-through;
-}
 </style>
---
+++ src/TaskList.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const props = defineProps(['tasklist'])
+function deleteTask(index: number){
+    props.tasklist.splice(index,1);
+}
+
+</script>
+
+<template>
+    <div v-auto-animate>
+        <div v-for="(item, index) in tasklist" class="taskItem">
+            <el-checkbox type="checkbox" v-model="item.isDone" />
+            <div :class="{isTaskDone: item.isDone}" class="grow">{{item.content}}</div>
+            <el-button type="danger" size="small" @click="deleteTask(index)"><i class="i-ant-design-delete-outlined"></i> 删除</el-button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.taskItem {
+    --at-apply: flex p-2 gap-x-2 items-center w-20em
+}
+
+.isTaskDone {
+    --at-apply: line-through;
+}
+</style>
