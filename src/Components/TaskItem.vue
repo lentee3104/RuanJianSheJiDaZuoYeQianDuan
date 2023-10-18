6aad9b5f886fd2134b7e1af2ea1e925f9f001902
sunxp <xpsun@qq.com>
2023-10-18 16:16:55 +0800

    feat: 增加了修改功能

--- src/Components/TaskItem.vue
+++ src/Components/TaskItem.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
-import {inject} from 'vue'
+import {inject,ref} from 'vue'
 import type {ITodoListService} from '@/Types/ITodoListService'
 
-defineProps(['item','itemIndex'])
-defineEmits(['deleteTask'])
-
+const props = defineProps(['item','itemIndex'])
 const todoListService = inject<ITodoListService>('todoListService')!
+
+const isEditMode = ref(false);
+
+const newContent = ref("");
+
+function onEdit(){
+    isEditMode.value=true
+    newContent.value= props.item.content
+}
+
+function onOk(){
+    isEditMode.value=false
+    props.item.content= newContent.value
+}
 </script>
 
 <template>
     <div  class="taskItem">
         <el-checkbox type="checkbox" v-model="item.isDone" />
-        <div :class="{isTaskDone: item.isDone}" class="grow">{{item.content}}</div>
-        <el-button type="danger" size="small" @click="todoListService.deleteTask(itemIndex)"><i class="i-ant-design-delete-outlined"></i> 删除</el-button>
+        <template v-if="isEditMode">
+            <el-input class="grow" v-model="newContent"/>
+            <el-button type="primary" size="small" @click="onOk">确定</el-button>
+            <el-button type="danger" size="small" @click="isEditMode=false">取消</el-button>
+        </template>
+        <template v-else>
+            <div :class="{isTaskDone: item.isDone}" class="grow">{{item.content}}</div>
+            <el-button type="primary" size="small" @click="onEdit">修改</el-button>
+            <el-button type="danger" size="small" @click="todoListService.deleteTask(itemIndex)"><i class="i-ant-design-delete-outlined"></i> 删除</el-button>
+        </template>
     </div>
 
 </template>
