4e0d8c2328f7f2530f198866487c85cb1fbd6e17
lentee <yuxinyang2003@126.com>
2023-12-13 23:38:25 +0800

    做了在这个页面实现加购和减购。减到0的时候从listOrderList删掉OrderList对象。从0开始加的时候push一个新OrderList对象进listOrderList

--- src/views/customer/customerSelectItemInShop.vue
+++ src/views/customer/customerSelectItemInShop.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
 
 import {useRoute} from "vue-router";
-import {onMounted, ref} from "vue";
+import {onMounted, ref, computed} from "vue";
 import axios from "axios";
 
 const route = useRoute()
 const shopId = parseInt(<string>route.query.shopId)
 const customerId = parseInt(localStorage.getItem("customerId") ?? '')
+const shopItemList = ref([])
 
 let orderTable = ref({})
+interface OrderList {
+  item_id: number,
+  item_quantity: number
+}
 
-const shopItemList = ref([])
+let listOrderList = ref<OrderList[]>([])
 
 onMounted(() => {
   getShopItem()
@@ -88,18 +93,60 @@ async function createOrderTable() {
   await getOrderTable()
 }
 
+// 定义计算属性，根据 item_id 查找对应对象的 item_quantity
+const getItemQuantity = (itemId: number) => {
+  const matchingObject = listOrderList.value.find(item => item.item_id === itemId);
+  if(matchingObject){
+    if(matchingObject.item_quantity == 0){
+      const matchingIndex = listOrderList.value.findIndex(item => item.item_id === itemId)
+      listOrderList.value.splice(matchingIndex, itemId);
+      return 0
+    }else {
+      return matchingObject.item_quantity
+    }
+  }
+  console.log(listOrderList.value)
+  return 0
+};
+
+/*减少数量*/
+const decreaseQuantity = (itemId: number) =>{
+  const matchingObject = listOrderList.value.find(item => item.item_id === itemId);
+  if (matchingObject && matchingObject.item_quantity!=0) {
+    matchingObject.item_quantity -= 1;
+  }
+}
+
+/*增加数量*/
+const increaseQuantity = (itemId: number) =>{
+  const matchingObject = listOrderList.value.find(item => item.item_id === itemId);
+  if (matchingObject) {
+    matchingObject.item_quantity += 1;
+  }else{
+    let orderList = ref<OrderList>({
+      item_id: 0,
+      item_quantity: 0
+    })
+    orderList.value.item_id = itemId
+    orderList.value.item_quantity = 1
+    listOrderList.value.push(orderList.value)
+  }
+}
+
 </script>
 
 <template>
   <div class="flex-col items-center ">
     <div>这是用户挑选商品页面</div>
-    <div v-for="shopItem in shopItemList" :key="shopItem.item.itemId" class="flex items-center gap-x-4">
+    <div v-for="shopItem in shopItemList" :key="shopItem.item.itemId"  class="flex items-center gap-x-4">
       <!--      <p>ID: {{ shopItem.id }}</p>
             <p>Shop ID: {{ shopItem.shop.shopId }}</p>
             <p>item ID: {{ shopItem.item.itemId }}</p>-->
       <p>item Name: {{ shopItem.item.itemName }}</p>
       <p>item Price: {{ shopItem.item.itemPrice }}</p>
-      <el-button @click="">加入购物车</el-button>
+      <el-button circle @click="decreaseQuantity(shopItem.item.itemId)">-</el-button>
+      <div>{{ getItemQuantity(shopItem.item.itemId) }}</div>
+      <el-button circle @click="increaseQuantity(shopItem.item.itemId)">+</el-button>
       <!-- 这里可以根据需要添加其他展示信息 -->
     </div>
   </div>
